4c0aeca1c0a4dbba823ca6567aace1c32878753e
Nataliia_Shulga <shulga.nataliia1@gmail.com>
2023-07-19 14:38:32 -0400

    updated LoginPageStepDefinition

--- src/test/java/com/next_base_crm/step_definitions/LoginPageStepsDefinition.java
+++ src/test/java/com/next_base_crm/step_definitions/LoginPageStepsDefinition.java
@@ -51,6 +51,61 @@ public class LoginPageStepsDefinition {
         BrowserUtils.verifyTitleContains("Portal");
     }
 
+    @When("user clicks on {string} button")
+    public void user_clicks_on_button(String string) {
+
+    }
+    @Then("user removes {string} default delivery option")
+    public void user_removes_default_delivery_option(String string) {
+
+    }
+    @Then("user types message title in message title input box")
+    public void user_types_message_title_in_message_title_input_box() {
+
+    }
+    @Then("user adds question in Question input box")
+    public void user_adds_question_in_question_input_box() {
+
+    }
+    @Then("user adds multiple answers in Answer input box")
+    public void user_adds_multiple_answers_in_answer_input_box() {
+
+    }
+
+    @Then("user sees error delivery message: {string}")
+    public void user_sees_error_delivery_message(String string) {
+
+    }
+
+    @Then("user types poll message title in message title input box")
+    public void user_types_poll_message_title_in_message_title_input_box() {
+
+    }
+    @Then("user sees error question message: {string}")
+    public void user_sees_error_question_message(String string) {
+
+    }
+
+    @Then("user sees the delivery {string} by default")
+    public void user_sees_the_delivery_by_default(String string) {
+
+    }
+
+    @Then("user clicks on {string} checkbox")
+    public void user_clicks_on_checkbox(String string) {
+
+    }
+
+    @Then("user sees error title message: {string}")
+    public void user_sees_error_title_message(String string) {
+
+    }
+
+    @Then("user sees error answer message: {string}")
+    public void user_sees_error_answer_message(String string) {
+
+    }
+
 
 }
 
