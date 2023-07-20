6a1205e64f1167f7e50e3b675fcced250a6242c9
Nataliia_Shulga <shulga.nataliia1@gmail.com>
2023-07-20 03:45:29 -0400

    updated step_deff

--- src/test/java/com/next_base_crm/step_definitions/LoginPageStepsDefinition.java
+++ src/test/java/com/next_base_crm/step_definitions/LoginPageStepsDefinition.java
@@ -10,6 +10,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class LoginPageStepsDefinition {
     LoginPage loginPage = new LoginPage();
@@ -50,34 +52,64 @@ public class LoginPageStepsDefinition {
 
     @Then("user should be on the activity stream")
     public void userShouldBeOnTheHomePage() {
+
         BrowserUtils.verifyTitleContains("Portal");
+
     }
 
     ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+
+
+    @Given("user is on the activity stream")
+    public void user_is_on_the_activity_stream() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        //loginPage.Hr_login();
+        //loginPage.helpDesk_login();
+        loginPage.marketing_login();
+
+    }
+
+    @When("user clicks on the {string} button")
+    public void user_clicks_on_the_button(String button1) {
+      activityStreamPage.pollButton.click();
+    }
+
+    @Then("user sees the delivery {string} by default")
+    public void user_sees_the_delivery_by_default(String deliveryByDefault) {
+        Assert.assertTrue(activityStreamPage.allEmployeeOption.isDisplayed());
+
+    }
+
+    @Then("user types {string} in message title input box")
+    public void user_types_in_message_title_input_box(String messageTitle) {
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        activityStreamPage.messageTitlePollInputBox.sendKeys("poll message");
+    }
+
     @When("user clicks on {string} button")
     public void user_clicks_on_button(String button) {
         activityStreamPage.sendButton.click();
+
     }
     @Then("user removes {string} default delivery option")
     public void user_removes_default_delivery_option(String removingButton) {
         activityStreamPage.removeAllEmployeeButton.click();
     }
-    @Then("user types message title in message title input box")
-    public void user_types_message_title_in_message_title_input_box() {
-        activityStreamPage.messageTitlePollInputBox.sendKeys("poll message");
 
-    }
-    @Then("user adds question in Question input box")
-    public void user_adds_question_in_question_input_box() {
-        activityStreamPage.questionInputBox.sendKeys("what is a maven?");
 
+    @Then("user adds {string} in Question input box")
+    public void user_adds_in_question_input_box(String question) {
+        Driver.getDriver().switchTo().parentFrame();
+       activityStreamPage.questionInputBox.sendKeys("what is a maven?");
     }
-    @Then("user adds multiple answers in Answer input box")
-    public void user_adds_multiple_answers_in_answer_input_box() {
+    @Then("user adds {string} in Answer input box")
+    public void user_adds_in_answer_input_box(String answer) {
+        Driver.getDriver().switchTo().parentFrame();
         activityStreamPage.answer1InputBox.sendKeys("a");
         activityStreamPage.answer2InputBox.sendKeys("b");
-
     }
+
+
 
     @Then("user sees error delivery message: {string}")
     public void user_sees_error_delivery_message(String message1) {
@@ -86,34 +118,51 @@ public class LoginPageStepsDefinition {
         Assert.assertEquals(expectedErrorDeliveryMessage,actualErrorDeliveryMessage);
     }
 
-    @Then("user types poll message title in message title input box")
-    public void user_types_poll_message_title_in_message_title_input_box() {
 
-    }
     @Then("user sees error question message: {string}")
-    public void user_sees_error_question_message(String string) {
-
+    public void user_sees_error_question_message(String question) {
+        String actualErrorMessage = activityStreamPage.errorQuestionTextMessage.getText();
+        String expectedErrorMessage = "The question text is not specified.";
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
     }
 
-    @Then("user sees the delivery {string} by default")
-    public void user_sees_the_delivery_by_default(String string) {
 
-    }
 
     @Then("user clicks on {string} checkbox")
-    public void user_clicks_on_checkbox(String string) {
+    public void user_clicks_on_checkbox(String checkbox) {
+        activityStreamPage.allowMultipleChoiceCheckbox.click();
 
     }
 
     @Then("user sees error title message: {string}")
-    public void user_sees_error_title_message(String string) {
-
+    public void user_sees_error_title_message(String errorTitleMessage) {
+        String actualErrorTitleMsg = activityStreamPage.errorMessageTitleMessage.getText();
+        String expectedErrorTitleMsg = "The message title is not specified";
+        Assert.assertEquals(expectedErrorTitleMsg, actualErrorTitleMsg);
     }
 
     @Then("user sees error answer message: {string}")
-    public void user_sees_error_answer_message(String string) {
+    public void user_sees_error_answer_message(String question) {
+        String actualErrorAnswerMsg = activityStreamPage.errorAnswerMessage.getText();
+        String expectedErrorAnswerMsg = "The question " + "\"what is a maven?\"" + " has no answers.";
+        Assert.assertEquals(expectedErrorAnswerMsg, actualErrorAnswerMsg);
 
     }
+
+    @Then("user clicks on a {string} button")
+    public void user_clicks_on_a_button(String button3) {
+      activityStreamPage.cancelPollButton.click();
+    }
+
+   /* @Then("user sees error answer message: {string}  {string} {string}")
+    public void user_sees_error_answer_message(String error1, String question, String error2) {
+        String actualErrorAnswerMsg = activityStreamPage.errorAnswerMessage.getText();
+        String expectedErrorAnswerMsg = "The question" + " what is a maven? " + "has no answers.";
+        Assert.assertEquals(expectedErrorAnswerMsg, actualErrorAnswerMsg);
+    }
+
+    */
+
 
 
 }
