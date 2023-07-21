49b79a8ccf5e4c382e6f5d8fa5a35062d0a415a3
Simona <simona.savic88@gmail.com>
2023-07-21 11:09:59 -0400

    Simona

---
+++ src/test/java/com/next_base_crm/pages/CompanyPage.java
@@ -0,0 +1,28 @@
+package com.next_base_crm.pages;
+
+import com.next_base_crm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class CompanyPage {
+
+    public CompanyPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//a[@title='Company']")
+    public WebElement companyBtn;
+
+    @FindBy(xpath = "//a[@class='main-buttons-item-link']")
+    public List<WebElement> allTopModules;
+
+    //text
+
+
+
+
+
+}
