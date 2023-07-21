dd4757b4cf9331825fad3661cb5423be205bc348
Simona <simona.savic88@gmail.com>
2023-07-21 11:08:37 -0400

    Simona

---
+++ src/test/java/com/next_base_crm/step_definitions/MessageFieldStepDefinition.java
@@ -0,0 +1,106 @@
+package com.next_base_crm.step_definitions;
+
+import com.next_base_crm.pages.LoginPage;
+import com.next_base_crm.pages.MessagePage;
+import com.next_base_crm.utilities.BrowserUtils;
+import com.next_base_crm.utilities.ConfigurationReader;
+import com.next_base_crm.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class MessageFieldStepDefinition {
+
+    LoginPage user = new LoginPage();
+    MessagePage messagePage = new MessagePage();
+    WebDriver driver;
+
+    @Given("User is logged in")
+    public void user_is_logged_in() {
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        user.Hr_login();
+
+    }
+
+
+    @When("user clicks on the {string} button")
+    public void user_clicks_on_the_button(String messageBtn) {
+
+        messagePage.messageBtn.click();
+
+    }
+
+
+    @Then("the user type message into the message box")
+    public void the_user_type_message_into_the_message_box() {
+
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        messagePage.inputMessage.sendKeys("Hello great team");
+
+        Driver.getDriver().switchTo().parentFrame();
+
+    }
+
+    @Then("User click send button")
+    public void user_click_send_button() {
+
+        messagePage.sendButton.click();
+
+    }
+
+
+    @Then("{string} button is selected by default")
+    public void button_is_selected_by_default(String option) {
+
+    }
+
+
+
+    @When("the user type the message into the message field")
+    public void the_user_type_the_message_into_the_message_field() {
+
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        messagePage.inputMessage.sendKeys("Hello great team");
+
+        Driver.getDriver().switchTo().parentFrame();
+        messagePage.sendButton.click();
+
+
+        BrowserUtils.waitFor(2);
+    }
+
+    @When("the user clicks the Cancel button")
+    public void the_user_clicks_the_cancel_button() {
+
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        messagePage.inputMessage.sendKeys("Good morning great team");
+
+        BrowserUtils.waitFor(3);
+
+        Driver.getDriver().switchTo().parentFrame();
+        messagePage.cancelButton.click();
+
+    }
+
+    @Then("the message should not be sent and 'Send message ...' text is visible again")
+    public void the_message_should_not_be_sent() {
+
+        messagePage.defaultMessageText.getText().equals("Send message …");
+
+        //text
+
+
+
+    }
+
+
+
+
+
+}
