4b464a91706a86f8e8a6c41d02fcbca62b3d96c8
emirayral <emirayral@icloud.com>
2023-07-21 11:03:50 -0400

    Employees Page

--- src/test/java/com/next_base_crm/step_definitions/LoginPageStepsDefinition.java
+++ src/test/java/com/next_base_crm/step_definitions/LoginPageStepsDefinition.java
@@ -1,5 +1,6 @@
 package com.next_base_crm.step_definitions;
 
+import com.next_base_crm.pages.EmployeesPage;
 import com.next_base_crm.pages.LoginPage;
 import com.next_base_crm.utilities.BrowserUtils;
 import com.next_base_crm.utilities.ConfigurationReader;
@@ -8,9 +9,16 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginPageStepsDefinition {
     LoginPage loginPage = new LoginPage();
+
+    EmployeesPage employeesPage = new EmployeesPage();
 
     @Given("User should navigate to the webPage")
     public void userShouldNavigateToTheWebPage() {
@@ -49,6 +57,43 @@ public class LoginPageStepsDefinition {
     @Then("user should be on the activity stream")
     public void userShouldBeOnTheHomePage() {
         BrowserUtils.verifyTitleContains("Portal");
+    }
+
+
+    @Given("the user is on the Activity Stream page")
+    public void the_user_is_on_the_activity_stream_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        loginPage.Hr_login();
+
+        BrowserUtils.verifyTitleContains("Portal");
+    }
+
+
+    @When("the user clicks on the Employees module")
+    public void the_user_clicks_on_the_employees_module() {
+
+        employeesPage.employeesButton.click();
+
+    }
+    @Then("the Company Structure tab is open by default")
+    public void the_company_structure_tab_is_open_by_default() {
+
+        BrowserUtils.verifyTitleContains("Company Structure");
+
+    }
+    @Then("the user should see the following modules:")
+    public void the_user_should_see_the_following_modules(List<String> expectedModules) {
+
+        List<String> actualModules = new ArrayList<>();
+
+        for (WebElement eachModule : employeesPage.allTopModules) {
+
+            actualModules.add(eachModule.getText());
+            System.out.println(eachModule.getText());
+        }
+
+        Assert.assertEquals(expectedModules,actualModules);
+
     }
 
 
