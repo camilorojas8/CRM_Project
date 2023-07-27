a47f359d8bdafcfb4cf169499b3ba65384b65392
Simona <simona.savic88@gmail.com>
2023-07-27 18:23:40 -0400

    US_278

---
+++ src/test/java/com/next_base_crm/pages/MessagePage.java
@@ -0,0 +1,33 @@
+package com.next_base_crm.pages;
+
+import com.next_base_crm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MessagePage {
+
+    public MessagePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//span[@id='feed-add-post-form-tab-message']")
+    public WebElement messageBtn;
+
+    @FindBy(xpath = "//body[@contenteditable='true']")
+    public WebElement inputMessage;
+
+    @FindBy(xpath = "//span[@data-type='groups']")
+    public WebElement  allEmployeesBtn;
+
+    @FindBy(xpath = "//button[@id='blog-submit-button-save']")
+    public WebElement sendButton;
+
+    @FindBy(xpath = "//button[@id='blog-submit-button-cancel']")
+    public WebElement cancelButton;
+
+    @FindBy(xpath = "//span[text()='Send message …']")
+    public WebElement defaultMessageText;
+
+
+}
